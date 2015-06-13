2e18f72bb495b76c653cfadfa8f40ea6ebab84a5
César López-Natarén <cesarn@mindtouch.com>
2015-06-13 13:15:04 -0700

    Schema inference driver

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace EventsSchemaGenerator {
+
+    //--- Types ---
+    class EventSchemaGenerator {
+
+        //--- Methods ---
+        public static void Main (string[] args) {
+            var infer = new XmlSchemaInference();
+            var schemaSet = new XmlSchemaSet();
+            foreach(var filename in args) {
+                using(var reader = XmlReader.Create(filename)) {
+                    schemaSet = infer.InferSchema(reader, schemaSet);
+                }
+            }
+            foreach(XmlSchema schema in schemaSet.Schemas()) {
+                schema.Write(Console.Out);
+            }
+        }
+    }
+}
